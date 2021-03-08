e3931ccb1c105377afcff49704b11402741fdfd0
Filippo Garota <filogarota101@gmail.com>
2021-03-08 15:15:31 +0100

    Inzio progetto

---
+++ palindorma/js/script.js
@@ -0,0 +1,13 @@
+/*Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma*/
+
+// chiedo all'utente di inserire una Parola
+
+var parola = prompt('Insierisci una parola');
+
+// creo la funzione per verificare se la parola è palindroma
+
+function palindroma (word, wordReverse) {
+  parola = word;
+
+}
