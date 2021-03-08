2c153f363ddcc89a759d1358c11f444b4cd17955
Filippo Garota <filogarota101@gmail.com>
2021-03-08 15:28:03 +0100

    Esercizio della parola palindroma

--- palindorma/js/script.js
+++ palindorma/js/script.js
@@ -5,9 +5,23 @@ Creare una funzione per capire se la parola inserita è palindroma*/
 
 var parola = prompt('Insierisci una parola');
 
+
 // creo la funzione per verificare se la parola è palindroma
 
 function palindroma (word, wordReverse) {
-  parola = word;
-
+  for (var i = 0; i < parola.length; i++) {
+    word = parola[i];
+  }
+  for (var i = parola.length - 1; i >= 0; i-- ) {
+    wordReverse = parola[i];
+  }
+  if (word == wordReverse) {
+    return parola = 'La parola è palindroma';
+  } else {
+    return parola = ' La parola non è palindroma';
+  }
 }
+
+var parolaPalindroma = palindroma();
+
+console.log(parolaPalindroma);
